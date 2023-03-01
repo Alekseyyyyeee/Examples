0dc0b6e52ee0d904a549abe2dec09abdafdefdce
Alekseyyyyeee <aleksey.vysockiy@list.ru>
2023-03-02 02:11:00 +0800

    двигаюсь дальше10

---
+++ Example010_methodarray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {11, 16, 25, 3, 4, 53, 16, 7, 16, 28, 9};
+
+int n = array.Length;
+int find = 16;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index = index + 1;
+}
